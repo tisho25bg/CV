066004e8bad30f0c2c36c9ba5080e83d9e91c3f7
unknown <tisho26bg@gmail.com>
2019-02-10 10:08:44 +0200

    comit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+function myFunction(id) {
+
+	switch (id) {
+		case id=1:
+			window.location.href = "home.html";
+			break;
+		case id=2:
+
+			window.location.href = "my-resume.html";
+			break;
+		case id=3:
+
+			window.location.href = "my-skills.html";
+			break;
+		case id=4:
+
+			window.location.href = "my-job.html";
+			break;
+		case id=5:
+			// document.write("5");
+			window.location.href = "home.html";
+			break;
+		case id=6:
+			// document.write("6");
+			window.location.href = "my-resume.html";
+			break;
+	}
+}
+
