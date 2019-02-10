5488ee66649dfc22686ea23f3f234c370760d142
unknown <tisho26bg@gmail.com>
2019-02-10 14:36:02 +0200

    bd

--- js/main.js
+++ js/main.js
@@ -9,23 +9,23 @@ function myFunction(id) {
 			break;
 		case 2:
 
-			window.location.replace("my-resume.html");
+			window.location.replace("http://htmlpreview.github.io/?https://github.com/tisho25bg/CV/blob/master/my-resume.html");
 			break;
 		case 3:
 
-			window.location.href = "my-skills.html";
+			window.location.replace("http://htmlpreview.github.io/?https://github.com/tisho25bg/CV/blob/master/my-skills.html");
 			break;
 		case 4:
 
-			window.location.href = "my-job.html";
+			window.location.replace("http://htmlpreview.github.io/?https://github.com/tisho25bg/CV/blob/master/my-job.html");
 			break;
 		case 5:
 			// document.write("5");
-			window.location.href = "home.html";
+			window.location.replace("http://htmlpreview.github.io/?https://github.com/tisho25bg/CV/blob/master/home.html");
 			break;
 		case 6:
 			// document.write("6");
-			window.location.href = "my-resume.html";
+			window.location.replace("http://htmlpreview.github.io/?https://github.com/tisho25bg/CV/blob/master/my-resume.html");
 			break;
 	}
 }
