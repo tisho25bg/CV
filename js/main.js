3662ce1144129be59e9515362f72619fa928af43
unknown <tisho26bg@gmail.com>
2019-02-10 13:20:11 +0200

    ждс

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
 function myFunction(id) {
 
 	switch (id) {
-		case id=1:
+		case 1:
 			window.location.href = "home.html";
 			break;
-		case id=2:
+		case 2:
 
 			window.location.href = "my-resume.html";
 			break;
-		case id=3:
+		case 3:
 
 			window.location.href = "my-skills.html";
 			break;
-		case id=4:
+		case 4:
 
 			window.location.href = "my-job.html";
 			break;
-		case id=5:
+		case 5:
 			// document.write("5");
 			window.location.href = "home.html";
 			break;
-		case id=6:
+		case 6:
 			// document.write("6");
 			window.location.href = "my-resume.html";
 			break;
